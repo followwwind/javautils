c3419d8c90a28c249c9360dbc20c3244172287d1
followwwind <18771933975@163.com>
2017-09-18 22:31:01 +0800

    更新zxing二维码工具类

---
+++ src/main/java/com/wind/common/Color.java
@@ -0,0 +1,19 @@
+package com.wind.common;
+
+/**
+ * 颜色参照表
+ */
+public interface Color {
+    int GRAY = 11184810;
+    int LIGHTGRAY = 12303291;
+    int DARKGRAY = 4473924;
+    int BLACK = 0;
+    int WHITE = 16777215;
+    int BLUE = 8947967;
+    int GREEN = 8978312;
+    int LIGHTBLUE = 12303359;
+    int LIGHTGREEN = 12320699;
+    int RED = 267946120;
+    int ORANGE = 16777096;
+    int LIGHTRED = 16759739;
+}
--- src/main/java/com/wind/qrcode/zxing/ZxingUtils.java
+++ src/main/java/com/wind/qrcode/zxing/ZxingUtils.java
@@ -6,8 +6,8 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.wind.common.Color;
 import com.wind.common.Const;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -25,15 +25,17 @@ public class ZxingUtils {
 
 
     /**
-     * 生成指定目标文件的二维码
-     * @param content 内容
-     * @param width 宽度
-     * @param height 高度
-     * @param filePath 图片文件路径
-     * @param imageType 图片类型
+     * 设置二维码的参数
+     * @param content
+     * @param width
+     * @param height
+     * @return
      */
-    public static void writeCode(String content, int width, int height, String filePath, String imageType){
-
+    private static BitMatrix getBitMatrix(String content, int width, int height){
+        /**
+         * 设置二维码的参数
+         */
+        BitMatrix bitMatrix = null;
         try {
             Map<EncodeHintType, Object> hints = new HashMap<>();
             // 指定纠错等级
@@ -42,13 +44,39 @@ public class ZxingUtils {
             //设置白边
             hints.put(EncodeHintType.MARGIN, 1);
             // 生成矩阵
-            BitMatrix bitMatrix = new MultiFormatWriter().encode(content,
-                    BarcodeFormat.QR_CODE, width, height, hints);
-            Path path = FileSystems.getDefault().getPath(filePath);
+            bitMatrix = new MultiFormatWriter().encode(content,BarcodeFormat.QR_CODE,width,height,hints);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return bitMatrix;
+    }
+
+    /**
+     * 生成指定目标文件的二维码
+     * @param content 内容
+     * @param width 宽度
+     * @param height 高度
+     * @param target 图片文件路径
+     * @param imageType 图片类型
+     */
+    public static void writeCode(String content, int width, int height, String imageType, String target){
+        // 生成矩阵
+        BitMatrix bitMatrix = getBitMatrix(content, width, height);
+        writeCode(bitMatrix, imageType, target);
+    }
+
+    /**
+     * 根据bitMatrix生成二维码
+     * @param bitMatrix
+     * @param target
+     * @param imageType
+     */
+    public static void writeCode(BitMatrix bitMatrix, String imageType, String target){
+        try {
+            Path path = FileSystems.getDefault().getPath(target);
             // 输出图像
             MatrixToImageWriter.writeToPath(bitMatrix, imageType, path);
-        } catch (WriterException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -58,69 +86,67 @@ public class ZxingUtils {
      * 生成正方形二维码
      * @param content 内容
      * @param length 边长
-     * @param filePath 图片文件路径
+     * @param target 图片文件路径
      * @param imageType 图片类型
      */
-    public static void writeCode(String content, int length, String filePath, String imageType){
-        writeCode(content, length, length, filePath, imageType);
+    public static void writeCode(String content, int length, String imageType, String target){
+        writeCode(content, length, length, imageType, target);
     }
 
 
     /**
      * 添加居中logo
-     * @param filePath
+     * @param content
      * @param imageType 图片类型
      * @param logo logo路径
      * @param logoLength
      * @param target 目标路径
      */
-    public static void addLogo(String filePath, String imageType, String logo, int logoLength, String target){
+    public static void writeCode(String content, int length, String imageType, String logo, int logoLength, String target){
 
-        File codeFile = new File(filePath);
+        BitMatrix matrix = getBitMatrix(content, length, length);
         File logoFile = new File(logo);
-
-        if(!codeFile.exists() || !logoFile.exists()){
-            System.out.println("文件未找到!!!");
-            return;
-        }
-        /**
-         * 读取二维码图片，并构建绘图对象
-         */
-        BufferedImage codeImage = null;
-        try {
-            codeImage = ImageIO.read(codeFile);
-            int codeWidth = codeImage.getWidth();
-            int codeHeight = codeImage.getHeight();
+        if(!logoFile.exists()){
+            writeCode(matrix, target, imageType);
+        }else{
+            int width = matrix.getWidth();
+            int height = matrix.getHeight();
+            BufferedImage codeImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);  ;
+            for (int x = 0; x < length; x++) {
+                for (int y = 0; y < length; y++) {
+                    codeImage.setRGB(x, y, matrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
             Graphics2D g = codeImage.createGraphics();
             /**
              * 读取Logo图片
              */
-            BufferedImage logoImage = ImageIO.read(logoFile);
+            try {
+                BufferedImage logoImage = ImageIO.read(logoFile);
+                int logoWidth = logoLength;
+                int logoHeight = logoLength;
+                /**
+                 * logo放在中心
+                 */
+                int x = (length - logoWidth) / 2;
+                int y = (length - logoHeight) / 2;
+                g.drawImage(logoImage, x, y, logoWidth, logoHeight, null);
+                g.dispose();
+                codeImage.flush();
+                logoImage.flush();
+                ImageIO.write(codeImage, imageType, new File(target));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-            int logoWidth = logoLength;
-            int logoHeight = logoLength;
-            /**
-             * logo放在中心
-             */
-            int x = (codeWidth - logoWidth) / 2;
-            int y = (codeHeight - logoHeight) / 2;
-            //开始绘制图片
-            g.drawImage(logoImage, x, y, logoWidth, logoHeight, null);
-            g.setColor(Color.BLACK);
-            g.setBackground(Color.WHITE);
-            g.dispose();
-            logoImage.flush();
-            codeImage.flush();
-
-            ImageIO.write(codeImage, imageType, new File(target));
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-
     }
 
-    public static void parseCode(String filePath){
+    /**
+     * 解析二维码
+     * @param filePath
+     */
+    public static void readCode(String filePath){
         BufferedImage image;
         try {
             image = ImageIO.read(new File(filePath));
@@ -140,9 +166,13 @@ public class ZxingUtils {
     }
 
     public static void main(String[] args) {
-//        writeCode("hello", 300, "E:\\3.png", Const.IMAGE_PNG);
-//        addLogo("E:\\3.png", Const.IMAGE_PNG, "E:\\resource\\picture\\3.png", 30, "E:\\1.png");
-//        parseCode("E:\\3.png");
+        String target = "src/main/resources/image/code.png";
+        /*String content = "hello world";
+        int length = 300;
+        String logo = "src/main/resources/image/head.jpg";
+        int logoLength = 50;
+        writeCode(content, length, Const.IMAGE_PNG, logo, logoLength, target);*/
+        readCode(target);
     }
 
 }
