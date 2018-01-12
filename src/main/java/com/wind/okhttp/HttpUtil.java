abf8a02e02b1f8523cc4ddd87ae196dfcdee7f60
followwwind <18771933975@163.com>
2018-01-12 17:34:18 +0800

    更新

--- src/main/java/com/wind/okhttp/HttpUtil.java
+++ src/main/java/com/wind/okhttp/HttpUtil.java
@@ -2,7 +2,11 @@ package com.wind.okhttp;
 
 import okhttp3.*;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * http请求工具类
@@ -47,28 +51,27 @@ public class HttpUtil {
 
 
     public static void main(String[] args) {
-        String url = "http://192.168.1.44:8181/hb/Advertisement/getPageList.json";
-        String json = "{}";
+        String url = "https://kyfw.12306.cn/passport/captcha/captcha-image?login_site=E&module=login" +
+                "&rand=sjrand&0.229965615247677";
+
+        Request request = new Request.Builder()
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36")
+                .addHeader("Referer", "https://kyfw.12306.cn/otn/login/init")
+                .url(url).build();
         try {
-            Response response = get("https://www.baidu.com").execute();
-            int code = response.code();
-            System.out.println(code);
-            System.out.println(response.body().string());
-            /*post(url, json).enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
+            Response response = client.newCall(request).execute();
+            ResponseBody body = response.body();
+            InputStream in = body.byteStream();
+            FileOutputStream out = new FileOutputStream("E:/work/img/1.png");
+            byte[] bytes = new byte[1024];
+            int len = -1;
+            while((len = in.read(bytes, 0, bytes.length)) != -1){
+                out.write(bytes, 0, len);
+            }
 
-                }
+            out.flush();
+            out.close();
 
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    int code = response.code();
-                    System.out.println(code);
-                    System.out.println(response.body().string());
-
-                    System.exit(0);
-                }
-            });*/
         } catch (IOException e) {
             e.printStackTrace();
         }
