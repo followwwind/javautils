45f416cd619ef1a4368f4b8a180a5730b8cbdaf3
chenlushun <chenlushun@51nbapi.com>
2019-04-05 17:47:19 +0800

    1、编译问题 2、添加身份证工具类 3、字符串工具类添加一个方法

--- src/main/java/com/wind/common/StringUtil.java
+++ src/main/java/com/wind/common/StringUtil.java
@@ -10,26 +10,29 @@ import java.util.StringTokenizer;
 
 /**
  * 字符串工具类
+ *
  * @author wind
  */
 public class StringUtil {
 
     /**
      * 判断字符串是否为null或空字符串
+     *
      * @param str
      * @return
      */
-    public static boolean isNotBlank(String str){
+    public static boolean isNotBlank(String str) {
         return str != null && !"".equals(str.trim());
     }
 
 
     /**
      * url编码
+     *
      * @param str
      * @return
      */
-    public static String encodeUrl(String str){
+    public static String encodeUrl(String str) {
         String s = null;
         try {
             s = URLEncoder.encode(str, Constants.UTF8);
@@ -42,10 +45,11 @@ public class StringUtil {
 
     /**
      * url解码
+     *
      * @param str
      * @return
      */
-    public static String decodeUrl(String str){
+    public static String decodeUrl(String str) {
         String s = null;
         try {
             s = URLDecoder.decode(str, Constants.UTF8);
@@ -58,18 +62,19 @@ public class StringUtil {
 
     /**
      * 字符串分隔 StringTokenizer效率是三种分隔方法中最快的
+     *
      * @param str
      * @param sign
      * @return
      */
-    public static String[] split(String str, String sign){
-        if(str == null){
+    public static String[] split(String str, String sign) {
+        if (str == null) {
             return new String[]{};
         }
-        StringTokenizer token = new StringTokenizer(str,sign);
+        StringTokenizer token = new StringTokenizer(str, sign);
         String[] strArr = new String[token.countTokens()];
         int i = 0;
-        while(token.hasMoreElements()){
+        while (token.hasMoreElements()) {
             strArr[i] = token.nextElement().toString();
             i++;
         }
@@ -78,13 +83,24 @@ public class StringUtil {
 
     /**
      * 字符串拼接
+     *
      * @param sign
      * @param strArr
      * @return
      */
-    public static String joinStr(String sign, String... strArr){
-        Optional<String> optional  = Arrays.stream(strArr).filter(Objects::nonNull
+    public static String joinStr(String sign, String... strArr) {
+        Optional<String> optional = Arrays.stream(strArr).filter(Objects::nonNull
         ).reduce((a, b) -> a + sign + b);
         return optional.orElse("");
+    }
+
+    /**
+     * if str is null or "", return true
+     *
+     * @param str str
+     * @return true/false
+     */
+    public static boolean isBlank(String str) {
+        return str == null || "".equals(str.trim());
     }
 }
